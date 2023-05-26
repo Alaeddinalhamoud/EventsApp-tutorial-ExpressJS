7e2f00f7ac18965fc2882f2763ba13c59b973d1d
IBRAHIM HASSEN <118390866+ibrahimmohammedhassen@users.noreply.github.com>
2023-05-26 20:06:30 +0100

    event ffinal

--- routes/event.js
+++ routes/event.js
@@ -12,9 +12,9 @@ console.log("Id" + id);
 
   const result = await db.promise().query(`SELECT Events.Id as Id, DATE_FORMAT( Events.Date, "%M %d %Y") as Date, TIME_FORMAT(Events.Time , "%H:%i") as Time, Performers.Name as PerformerName, Performers.ImageUrl as PerformerImageUrl, Stages.Name as StageName, Stages.ImageUrl as StageImageUrl FROM Events INNER JOIN Performers ON Performers.Id = Events.PerformareId INNER JOIN Stages ON Stages.Id = Events.StageId WHERE Events.Id = ${id}`);
 
-   console.log(result[0]);
+   console.log(result[0][0]);
  
-  res.render("event", {events: result[0]});
+  res.render("event", {event: result[0][0]});
 });
 
 
--- routes/stage.js
+++ routes/stage.js
@@ -10,7 +10,20 @@ router.get("/", async (req, res) => {
 
    console.log(result[0]);
  
-  res.render("stage", {stages: result[0]});
+  res.render("stages", {stages: result[0]});
 });
+
+router.get("/:id", async (req, res) => {
+  console.log("Get stage by Id")
+  const id = parseInt(req.params.id)
+  
+  console.log("Id" + id);
+  
+    const result = await db.promise().query(`SELECT Events.Id as Id, DATE_FORMAT( Events.Date, "%M %d %Y") as Date, TIME_FORMAT(Events.Time , "%H:%i") as Time, Performers.Name as PerformerName, Performers.ImageUrl as PerformerImageUrl, Stages.Name as StageName, Stages.ImageUrl as StageImageUrl FROM Events INNER JOIN Performers ON Performers.Id = Events.PerformareId INNER JOIN Stages ON Stages.Id = Events.StageId WHERE Events.StageId = ${id}`);
+  
+     console.log(result[0][0].StageName);
+   
+    res.render("stage", {stages: result[0], stageName:result[0][0].StageName});
+  });
 
 module.exports = router;
